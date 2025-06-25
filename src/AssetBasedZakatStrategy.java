eaf4c809df7ec292bc6d16ee23e8434b02697564
Shery-M <162703972+Shery-M@users.noreply.github.com>
2025-06-25 18:10:43 +0200

    Update AssetBasedZakatStrategy.java

--- src/AssetBasedZakatStrategy.java
+++ src/AssetBasedZakatStrategy.java
@@ -1,1 +1,18 @@
---
+package com.shrouk;
+
+
+public class AssetBasedZakatStrategy implements IZakatStrategy {
+    private static final double NISAB = 595 * 3.75;
+
+    @Override
+    public double calculateZakat(double totalAssets) {
+        if (totalAssets < NISAB) return 0;
+        return totalAssets * 0.025;
+    }
+
+    @Override
+    public double getNisab() {
+        return NISAB;
+    }
+}
+
